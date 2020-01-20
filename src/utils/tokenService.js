8e25cc4fc417ff13e20ecdf03e89df5f8ea04af3
devan <devanrterry@gmail.com>
2020-01-19 21:14:35 -0800

    completed step 3

---
+++ src/utils/tokenService.js
@@ -0,0 +1,11 @@
+export default {
+    setToken
+};
+
+function setToken(token) {
+    if (token) {
+        localStorage.setItem('token', token)
+    } else {
+        localStorage.removeItem('token')
+    }
+}
