4692f95cf052c6d4021325fa9a64731f268c055b
devan <devanrterry@gmail.com>
2020-01-22 09:24:30 -0800

    set up postservice file

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,5 +1,10 @@
 const User = require('../models/user');
 
 module.exports = {
-
+    create
 }
+
+async function create(req, res) {
+    const post = await Post.create(req.body);
+    res.status(201).json(puppy);
+  }
